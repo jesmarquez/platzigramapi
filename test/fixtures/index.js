17834ca501200b04a6a4402cf3d148b4032aec0a
jesmarquez <jesmarquezacevedo@yahoo.es>
2017-03-14 15:56:38 -0500

    creacion endpoint GET/picture

---
+++ test/fixtures/index.js
@@ -0,0 +1,13 @@
+export default {
+  getImage () {
+    return {
+      id: '6ab79620-08c7-11e7-a87e-adfa0e4d03b7',
+      publicId: '2qY9COoAhfMrsH7mCyh86T',
+      userId: 'platzigram',
+      src: 'http://platzigram.test/3ewrewrr.jpg',
+      descripttion: '#awesome',
+      tags: ['awesome'],
+      createAt: new Date().toString()
+    }
+  }
+}
