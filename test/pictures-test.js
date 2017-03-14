90602752a13c8211f89da8c3b29bfc1541caeb34
jesmarquez <jesmarquezacevedo@yahoo.es>
2017-03-14 16:32:16 -0500

    creacion endpoint POST/picture

--- test/pictures-test.js
+++ test/pictures-test.js
@@ -7,14 +7,39 @@ import request from 'request-promise'
 import pictures from '../pictures'
 import fixtures from './fixtures'
 
+test.beforeEach(async t => {
+  let srv = micro(pictures)
+  t.context.url = await listen(srv)
+})
+
 test('GET /:id', async t => {
   let image = fixtures.getImage()
-  let srv = micro(pictures)
-  let url = await listen(srv)
+  let url = t.context.url
 
   let body = await request({ uri: `${url}/${image.publicId}`, json: true })
   t.deepEqual(body, image)
 })
 
-test.todo('POST /')
+test('POST /', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body: {
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    resolveWithFullResponse: true
+  }
+
+  let response = await request(options)
+
+  t.is(response.statusCode, 201)
+  t.deepEqual(response.body, image)
+})
+
 test.todo('POST /:id/like')
