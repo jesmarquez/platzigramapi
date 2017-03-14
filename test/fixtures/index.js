c63a11e3b3ca36db83c0e0819b5912e6b2b4aa7f
jesmarquez <jesmarquezacevedo@yahoo.es>
2017-03-14 17:23:19 -0500

    creacion endpoint POST/picture like

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -4,6 +4,8 @@ export default {
       id: '6ab79620-08c7-11e7-a87e-adfa0e4d03b7',
       publicId: '2qY9COoAhfMrsH7mCyh86T',
       userId: 'platzigram',
+      liked: false,
+      likes: 0,
       src: 'http://platzigram.test/3ewrewrr.jpg',
       descripttion: '#awesome',
       tags: ['awesome'],
