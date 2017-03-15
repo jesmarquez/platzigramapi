05147a2e27c07e0170c56f004f9d674fa1bbe50c
jesmarquez <jesmarquezacevedo@yahoo.es>
2017-03-15 17:55:53 -0500

    create POST /user

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -26,5 +26,16 @@ export default {
       this.getImage(),
       this.getImage()
     ]
+  },
+
+  getUser () {
+    return {
+      id: 'uye3989432-ewre-3232-erv332ffg',
+      name: 'Freddy Vega',
+      username: 'freddier',
+      email: 'f@platzi.test',
+      password: 'pl4tzi',
+      createAt: new Date().toString()
+    }
   }
 }
