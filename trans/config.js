190075973428120dab95c85b575ec27f4a22fb3a
jesmarquez <jesmqz@gmail.com>
2017-04-17 14:40:39 -0500

    delete directory transpilation

--- trans/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  db: {},
-  secret: process.env.PLATZIGRAM_SECRET || 'platzi4' // no usar defaults
-};
